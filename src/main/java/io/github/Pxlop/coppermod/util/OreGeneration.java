4d492f4bbedc5c6421b3b78f41f08ee5ee24fbc8
Pxlop <43133000+Pxlop@users.noreply.github.com>
2020-08-08 17:09:08 +0800

    Updated to v1.0.2
    Updated to v1.0.2

--- src/main/java/io/github/Pxlop/coppermod/util/OreGeneration.java
+++ src/main/java/io/github/Pxlop/coppermod/util/OreGeneration.java
@@ -2,6 +2,7 @@ package io.github.Pxlop.coppermod.util;
 
 import io.github.Pxlop.coppermod.CopperMod;
 import io.github.Pxlop.coppermod.init.ModBlocks;
+import net.minecraft.block.BlockState;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
@@ -14,7 +15,11 @@ public class OreGeneration {
     public static void addOverworldOres() {
         for(Biome biome: ForgeRegistries.BIOMES) {
             CopperMod.LOGGER.debug("Adding copper ore to: " + biome);
-            biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.COPPER_ORE.get().getDefaultState(), 8)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(16, 0, 0, 128))));
+            addOre(biome, OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.COPPER_ORE.get().getDefaultState(), 8, 16, 7, 0, 60);
         }
+    }
+    
+    public static void addOre(Biome biome, OreFeatureConfig.FillerBlockType fillerBlock, BlockState defaultBlockState, int size, int count, int bottomOffset, int topOffset, int max) {
+        biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(new OreFeatureConfig(fillerBlock, defaultBlockState, size)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(count, bottomOffset, topOffset, max))));
     }
 }
